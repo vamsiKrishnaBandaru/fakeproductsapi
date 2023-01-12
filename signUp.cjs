95ae1ffee0b167e997577320d991ed9913b517f5
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-01-12 12:04:05 +0530

    added error msgs and border colours

--- signUp.cjs
+++ signUp.cjs
@@ -1,27 +1,75 @@
-const form = document.getElementById('signup-form');
+const form = document.getElementById('signup-form')
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
 const password = document.getElementById('password');
 const passwordRepeat = document.getElementById('password-repeat');
-const tos = document.getElementById('tos');
+// const tos = document.getElementById('tos');
+
+
+const firstNameSec = document.querySelector('first-name');
+
+// const firstNameerrorMessage = document.querySelector('firstNameerrorMessage')
+
+// console.log(firstNameerrorMessage)
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  let isFormValid = true;
-  const errorMessage = document.createElement('div');
-  errorMessage.style.color = 'red';
 
-  if (password.value !== passwordRepeat.value) {
-    errorMessage.textContent = 'Passwords do not match';
-    form.appendChild(errorMessage);
-    isFormValid = false;
+  if (firstName.value === '' || checkName(firstName.value)) {
+    errorOcuured(firstName, 'Please enter name using letters only')
+  } else {
+    borderGreen(firstName)
   }
 
-  if (!tos.checked) {
-    errorMessage.textContent = 'You must accept the terms of service';
-    form.appendChild(errorMessage);
-    isFormValid = false;
+  if (lastName.value === '' || checkName(lastName.value)) {
+    errorOcuured(lastName, 'Please enter name using letters only')
+  } else {
+    borderGreen(lastName)
   }
 
-  if (isFormValid) {
-    form.submit();
+  if (email.value == '' || checkEmail(email.value)) {
+    errorOcuured(email, 'Invalid email address')
+  } else {
+    borderGreen(email)
   }
-});
+  if (password.value == '' || checkEmail(password.value)) {
+    errorOcuured(password, 'Your password must be at least 8 to 16 characters')
+  } else {
+    borderGreen(password)
+  }
+  if (passwordRepeat.value == '' || checkEmail(passwordRepeat.value)) {
+    errorOcuured(passwordRepeat, 'Password must be same')
+  } else {
+    borderGreen(passwordRepeat)
+  }
+})
+
+function checkName(name) {
+  const checkCharectors = ['~', '!', '@', '#', '$', '%', '*', '(', ')', '+', '=', ':']
+  if (name.includes(checkCharectors)) {
+    return false
+  }
+}
+
+
+function checkEmail(email) {
+  var check = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  if (!email.match(check)) {
+    return true
+  }
+  return false
+}
+
+function errorOcuured(element, text) {
+  element.nextElementSibling.textContent = text
+  element.nextElementSibling.style.color = 'red'
+  element.style.border = '1px solid red'
+
+}
+
+
+function borderGreen(element) {
+  element.nextElementSibling.textContent = ""
+  element.style.border = '1px solid green'
+}
